854a646f6146eab4aa4f9d87043d230603a59369
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-09-29 21:32:37 -0700

    added Form component

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -10,37 +10,37 @@ function TableHeader() {
     );
   }
   
-  function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-        return (
-            <tr key={index}>
-            <td>{row.name}</td>
-            <td>{row.job}</td>
-            <td>
-              <button onClick={() => props.removeCharacter(index)}>
-                Delete
-              </button>
-            </td>
-          </tr>
-        );
-       }
-      );
-      return (
-          <tbody>
-            {rows}
-           </tbody>
-       );
-    }
-  
-  function Table(props) {
+function TableBody(props) {
+const rows = props.characterData.map((row, index) => {
     return (
-      <table>
-        <TableHeader />
-        <TableBody
-          characterData={props.characterData}
-          removeCharacter={props.removeCharacter}
-        />
-      </table>
+        <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+            <button onClick={() => props.removeCharacter(index)}>
+            Delete
+            </button>
+        </td>
+        </tr>
     );
-  }
-  export default Table;
+    }
+    );
+    return (
+        <tbody>
+        {rows}
+        </tbody>
+    );
+}
+  
+function Table(props) {
+return (
+    <table>
+    <TableHeader />
+    <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+    />
+    </table>
+);
+}
+export default Table;
