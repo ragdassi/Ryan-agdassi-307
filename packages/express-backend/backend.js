ab71bc25fb75bbbd1f056decb78e2d3f4ccbc281
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-10-09 15:49:04 -0700

    get indivdual user by user ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,9 @@ const findUserByName = (name) => {
     );
   };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 
 app.use(express.json());
 
@@ -58,6 +61,16 @@ if (name != undefined) {
     res.send(users);
 }
 });
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
 
 app.listen(port, () => {
   console.log(
