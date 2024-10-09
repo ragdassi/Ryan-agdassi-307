0202825f6ada02ee063326a2b305a5829e12cf0d
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-10-09 15:39:20 -0700

    Get individual user

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+
 const users = {
     users_list: [
       {
@@ -33,18 +34,34 @@ const users = {
 const app = express();
 const port = 8000;
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+  
 app.get("/users", (req, res) => {
+const name = req.query.name;
+if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+} else {
     res.send(users);
-  });
+}
+});
 
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
 });
+
